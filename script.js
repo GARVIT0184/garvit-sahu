9f14e4c9a51441f4f430ae4c0d889109782caae8
24f2006741 <24f2006741@ds.study.iitm.ac.in>
2026-07-12 22:49:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
+
+    if(window.scrollY > 50){
+        nav.style.background = "#0d1117";
+    }else{
+        nav.style.background = "rgba(13,17,23,.9)";
+    }
+});
